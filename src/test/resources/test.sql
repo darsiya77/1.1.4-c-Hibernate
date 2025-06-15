8dcb22849db737f7dafd8a0cae94127a9613ed64
Andrey T <andrey.troyan77@gmail.com>
2025-06-15 19:11:37 +0300

    - Добавил папку src/test/resources и в него файл test.sql - в нем можно работать с базой данных MySQL и в этом файле писать SQL запросы в базу и вытаскивать таблицы данных; - изменил username - git config --global user.name <Andrey T>

---
+++ src/test/resources/test.sql
@@ -0,0 +1,9 @@
+#insert into users(name, age, email) values('Andrey', 35, 'andrey_troyan@mail.ru');
+#select * from users;
+#select * from users where name = 'Steve';
+#update users set name = 'Steve', age = 55 where id = 1
+# delete from users where id = 4;
+
+# https://youtu.be/U4sLRjmP0WQ?si=SFI_ns9GCRWW8AFW&t=251
+
+# в разделе Database выбираем users -> F4 - вытаскиваем из базы SQL табличку
