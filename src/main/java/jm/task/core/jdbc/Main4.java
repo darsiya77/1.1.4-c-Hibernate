3581695d5d181475a6faccfeb0babb0424673d4d
Andrey T <andrey.troyan77@gmail.com>
2025-07-01 19:24:13 +0300

    - Update Util.class + Hibernate settings

--- src/main/java/jm/task/core/jdbc/Main4.java
+++ src/main/java/jm/task/core/jdbc/Main4.java
@@ -7,28 +7,13 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Main {
+public class Main4 {
 
     public static void main(String[] args) {
 
 //        UserService userService = new UserServiceImpl();
 //        userService.dropUsersTable();
 //        userService.createUsersTable();
-//        userService.saveUser("Andrey", "Troyan", (byte) 35);
-//        userService.saveUser("Jenya", "Reshetnikov", (byte) 25);
-//        userService.saveUser("Daniil", "Ivanov", (byte) 27);
-//        userService.saveUser("Katya", "Romanova", (byte) 28);
-//
-//        System.out.println(userService.getAllUsers());
-//
-//        userService.removeUserById(3);
-//
-//        System.out.println(userService.getAllUsers());
-//
-//        userService.cleanUsersTable();
-//
-//        System.out.println(userService.getAllUsers());
-
 
         Session session;
         try (SessionFactory factory = new Configuration()  // SessionFactory - всегда надо закрывать!
@@ -48,5 +33,6 @@ public class Main {
 
 
         }
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
 
@@ -10,6 +18,10 @@ public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "Kata263!";
 
+    private Util() {
+    }
+
+//    for JDBC
     public static Connection getConnection() {
         try {
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
@@ -22,8 +34,45 @@ public class Util {
         }
     }
 
+//    for Hibernate
+    private static SessionFactory sessionFactory;
 
-    private Util() {
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                // Настройки Hibernate, эквивалентные hibernate.cfg.xml
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/myfirstbd?useSSL=false");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "Kata263!");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+                settings.put(Environment.POOL_SIZE, "5");
+
+                // Для более новых версий Hibernate (5.4+)
+                settings.put(Environment.JPA_JDBC_DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.JPA_JDBC_URL, "jdbc:mysql://localhost:3306/myfirstbd");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties())
+                        .build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Failed to create Hibernate SessionFactory", e);
+            }
+        }
+        return sessionFactory;
     }
-    // реализуйте настройку соеденения с БД
+
 }
